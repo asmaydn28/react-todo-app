b1d6f9fceca885e9d0a9e16c709efb0193a1d2a0
ASIM AYDIN <133012227+asmaydn28@users.noreply.github.com>
2025-05-06 16:45:00 +0300

    responsive update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Form from "react-bootstrap/Form";
-import { Button } from "react-bootstrap";
+import { Button, Col, Row } from "react-bootstrap";
 import "./App.css";
 import styled from "styled-components";
 
@@ -51,39 +51,41 @@ function App() {
   return (
     <>
       <section>
-        <div className="col-4 m-3 mx-auto">
-          <div className="text-center my-3 h1 fw-semibold">Yapılacaklar Listesi</div>
-          <div>
-            <Form className="d-flex">
-              <Form.Control
-                onChange={handleChange}
-                placeholder="Bir Görev Giriniz..."
-                value={todoInput}
-                className="text-center rounded-0 border-end-0"
-                type="text"
-              />
-              <Button onClick={addTodo} className="rounded-0">
-                Ekle
-              </Button>
-            </Form>
-          </div>
-          <div className="text-center fw-semibold mt-1 h6">
-            Girilen Değer: {todoInput}
-          </div>
-          <div className="d-flex justify-content-center">
-            <StyledUl className="w-100 text-center fw-semibold">
-              {todos.map((todo, index) => (
-                <StyledLi
-                  onClick={() => deleteTodo(index)}
-                  style={{ cursor: "pointer" }}
-                  key={index}
-                >
-                  {todo}
-                </StyledLi>
-              ))}
-            </StyledUl>
-          </div>
-        </div>
+        <Row>
+          <Col xl="4" lg="6" sm="8" xs="10" className="m-3 mx-auto">
+            <div className="text-center my-3 h1 fw-semibold">Yapılacaklar Listesi</div>
+            <div>
+              <Form className="d-flex">
+                <Form.Control
+                  onChange={handleChange}
+                  placeholder="Bir Görev Giriniz..."
+                  value={todoInput}
+                  className="text-center rounded-0 border-end-0"
+                  type="text"
+                />
+                <Button onClick={addTodo} className="rounded-0">
+                  Ekle
+                </Button>
+              </Form>
+            </div>
+            <div className="text-center fw-semibold mt-1 h6">
+              Girilen Değer: {todoInput}
+            </div>
+            <div className="d-flex justify-content-center">
+              <StyledUl className="w-100 text-center fw-semibold">
+                {todos.map((todo, index) => (
+                  <StyledLi
+                    onClick={() => deleteTodo(index)}
+                    style={{ cursor: "pointer" }}
+                    key={index}
+                  >
+                    {todo}
+                  </StyledLi>
+                ))}
+              </StyledUl>
+            </div>
+          </Col>
+        </Row>
       </section>
     </>
   );
